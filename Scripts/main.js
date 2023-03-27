c71b2a48a9f84c0680f8c59aa4b74dd5870f9d7a
EvgenPie <nakertashgames@gmail.com>
2023-03-27 13:53:59 +0300

    Fix

--- Scripts/main.js
+++ Scripts/main.js
@@ -24,5 +24,5 @@ LoadLayout("MainMenu").then(()=>{
     $("#title").text("Haha classic");
     $("#icon").attr("src","Icon.png");
     player.healthChanged.push(function (){$("#icon").width($("#icon").width()*(player.health/player.maxHealth));});
-    setInterval(function (){ player.TakeDamage(5)},1000);
+    setInterval(function (){ player.TakeDamage(1)},100);
 });
